60c2103aff41da2df7105b29db198b1aaba3fd0a
Yannik Behr <yannik@yanmail.de>
2026-03-26 00:17:38 +0000

    rename

--- tests/test_forecast.py
+++ tests/test_forecast.py
@@ -19,6 +19,16 @@ def test_forecasts(tmp_path, setup_simulated_data):
     assert xds.probs.max() <= 1
 
 
+def test_forecasts_w_real_data(tmp_path, setup_real_data):
+    data = setup_real_data
+    xds = forecast(
+        data, pew=30, bins=(0, 5, 20, 80, 95, 100), smoothing=30, compute_score=True
+    )
+    assert xds.probs.shape[0] == 1000
+    assert xds.probs.min() >= 0
+    assert xds.probs.max() <= 1
+
+
 def test_sensitivity_analysis(setup_real_data):
     data = setup_real_data
 
